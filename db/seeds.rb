9b36bae3bcf9f20a52cbb4cf073fbc8a65c5b0c1
MrCorba <jespercorba91@gmail.com>
2018-05-11 20:41:42 +0200

    Add client seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
 # frozen_string_literal: true
+
+5.times do
+  Client.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    company_name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    zipcode: Faker::Address.zip,
+    city: Faker::Address.city,
+    country: Faker::Address.country,
+    phonenumber: Faker::PhoneNumber.phone_number,
+    email: Faker::Internet.email
+  )
+end
